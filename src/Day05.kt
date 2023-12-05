3ecbb66b9cc0fe30ca629ec9b6a770d0e9f4f91e
Bartosz Grałek <bartosz.gralek@finanteq.com>
2023-12-05 23:19:46 +0100

    Day05 - part2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,15 +1,70 @@
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.system.measureTimeMillis
+
 fun main() {
 
     data class Single(
         val offset: Long,
         val range: LongRange
-    )
+    ) : Comparable<Single> {
+        override fun compareTo(other: Single): Int {
+            return when(this.range.first - other.range.first) {
+                in 1..Long.MAX_VALUE -> 1
+                0L -> return 0
+                else -> return -1
+            }
+        }
+    }
 
-    data class Mapper(
-        val singles: List<Single>
-    ) {
+    fun LongRange.intersect(other: LongRange): Triple<LongRange, LongRange, LongRange> {
+        return Triple(
+            this.first..<min(other.first, this.last + 1L),
+            max(this.first, other.first)..min(this.last, other.last),
+            max(other.last + 1L, this.first)..this.last
+        )
+    }
+
+    fun LongRange.plus(value: Long): LongRange {
+        return (first+value)..(last+value)
+    }
+
+    data class Mapper(val singles: List<Single>) {
+
         fun map(number: Long): Long {
             return singles.firstOrNull { number in it.range }?.let { number + it.offset } ?: number
+        }
+
+        fun map(range: LongRange): List<LongRange> {
+            val list = mutableListOf<LongRange>()
+            var restOfRange = range
+            val iterator = singles.iterator()
+            while (iterator.hasNext()) {
+                val single = iterator.next()
+                val intersect = restOfRange.intersect(single.range)
+
+                if (intersect.second.isEmpty()) {
+                    if (!intersect.first.isEmpty()) {
+                        list.add(intersect.first)
+                        return list
+                    }
+                    restOfRange = intersect.third
+                } else {
+                    if (!intersect.first.isEmpty()) {
+                        list.add(intersect.first)
+                    }
+                    list.add(intersect.second.plus(single.offset))
+
+                    if (intersect.third.isEmpty()) return list
+                    else restOfRange = intersect.third
+                }
+                if (!iterator.hasNext()) {
+                    list.add(restOfRange)
+                    return list
+                }
+            }
+
+            return list
         }
     }
 
@@ -29,12 +84,12 @@ fun main() {
                         )
                     )
                     if (!iterator.hasNext()) {
-                        add(Mapper(tmp.toList()))
+                        add(Mapper(tmp.sorted()))
                         tmp.clear()
                     }
                 } else {
                     if (tmp.isNotEmpty()) {
-                        add(Mapper(tmp.toList()))
+                        add(Mapper(tmp.sorted()))
                         tmp.clear()
                     }
                 }
@@ -53,15 +108,25 @@ fun main() {
         return seeds.min()
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        var seeds = digitRx.findAll(input.removeFirst())
+            .map { it.value.toLong() }
+            .chunked(2)
+            .map { (first, length) -> first..<first+length }
+            .toList()
+        val mappers = buildMappers(input)
+
+        mappers.forEach { mapper ->
+            seeds = seeds.flatMap { mapper.map(it) }
+        }
+
+        return seeds.minBy { it.first }.first
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    check(part1(testInput) == 35L)
+    check(part2(testInput) == 46L)
 
     val input = readInput("Day05")
-    part1(input).println()
-//    part2(input).println()
+    measureTimeMillis { part2(input).println() }.println()
+
 }
